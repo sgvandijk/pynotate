e40d19b276011d0edf91e4e5a18728d1ae8436ae
Sander van Dijk <sgvandijk@gmail.com>
2017-01-25 22:01:58 +0100

    Handle alpha channel

--- pynotate/io.py
+++ pynotate/io.py
@@ -1,4 +1,8 @@
 from skimage.io import imread
 
-def read_image(path):
-    return imread(path)
+
+def read_image(path, keep_alpha=False):
+    image = imread(path)
+    if image.shape[2] > 3 and not keep_alpha:
+        image = image[:, :, :3]
+    return image
--- tests/test_io.py
+++ tests/test_io.py
@@ -7,5 +7,10 @@ class TestIO(unittest.TestCase):
 
     def test_read_image(self):
         image = read_image('resources/20160430-17:33:03.png')
-
         self.assertEqual((480, 640, 3), image.shape)
+
+        image = read_image('resources/colors.png')
+        self.assertEqual((200, 200, 3), image.shape)
+
+        image = read_image('resources/colors.png', keep_alpha=True)
+        self.assertEqual((200, 200, 4), image.shape)
