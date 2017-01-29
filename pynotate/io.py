e12db530fb85d40bb0e2d79901042b4b335ebac8
Sander van Dijk <sgvandijk@gmail.com>
2017-01-29 21:07:42 +0000

    Docstring

--- pynotate/io.py
+++ pynotate/io.py
@@ -2,6 +2,17 @@ from skimage.io import imread
 
 
 def read_image(path, keep_alpha=False):
+    """reads image from disk
+
+    :param path: image file path
+    :param keep_alpha: whether to keep or discard alpha channel if present
+
+    :type path: str
+    :type keep_alpha: bool
+
+    :return: the loaded image
+    :rtype: numpy.ndarray
+    """
     image = imread(path)
     if image.shape[2] > 3 and not keep_alpha:
         image = image[:, :, :3]
